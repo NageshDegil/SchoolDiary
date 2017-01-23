f3f5ab85b318e405fc65fc134fd7a492cf22e653
pawan singh chauhan <pawansinghchouhan052gmail.com>
2017-01-23 10:27:58 +0530

    crash fix

--- app/src/main/java/com/pawan/schooldiary/splashScreen/activity/SplashScreenActivity.java
+++ app/src/main/java/com/pawan/schooldiary/splashScreen/activity/SplashScreenActivity.java
@@ -6,7 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.pawan.schooldiary.R;
-import com.pawan.schooldiary.registerOrLogin.activity.RegisterOrLoginActivity;
+import com.pawan.schooldiary.registerOrLogin.activity.RegisterOrLoginActivity_;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -18,7 +18,7 @@ public class SplashScreenActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashScreenActivity.this, RegisterOrLoginActivity.class);
+                Intent intent = new Intent(SplashScreenActivity.this, RegisterOrLoginActivity_.class);
                 startActivity(intent);
             }
         }, 1000);
